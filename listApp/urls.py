33d28db61113d99325bcc51dcd0a563821db3811
Jev Gore-Samad <jevgore-samad@Jevs-MacBook-Pro-2.local>
2024-07-25 20:28:48 +0100

    new page, needs items but we will get there

--- listApp/urls.py
+++ listApp/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.list_list, name='list_list'),
+    path('list/<int:pk>/', views.list_detail, name='list_detail'),
 ]
--- listApp/views.py
+++ listApp/views.py
@@ -1,7 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import List
 
 # Create your views here.
 def list_list(request):
     lists = List.objects.all()
     return render(request, 'listApp/list_list.html', {'lists': lists})
+
+def list_detail( request, pk):
+    list = get_object_or_404( List, pk=pk)
+    return render(request, 'listApp/list_detail.html', {'list': list})
