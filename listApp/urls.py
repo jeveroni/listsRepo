27e73623c77011e1a7ffad6640a3ca49aae3f527
Jev Gore-Samad <jevgore-samad@Jevs-MacBook-Pro-2.local>
2024-07-31 21:32:59 +0100

    new and edit on the page

--- listApp/urls.py
+++ listApp/urls.py
@@ -5,4 +5,6 @@ from . import views
 urlpatterns = [
     path('', views.list_list, name='list_list'),
     path('list/<int:pk>/', views.list_detail, name='list_detail'),
+    path('item/new', views.item_new, name='item_new'),
+    path('item/<int:pk>/edit', views.item_edit, name='item_edit'),
 ]
--- listApp/views.py
+++ listApp/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render, get_object_or_404
 from .models import List, Item
+from.forms import ItemForm
+from django.utils import timezone
+from django.shortcuts import redirect
 
 # Create your views here.
 def list_list(request):
@@ -10,3 +13,27 @@ def list_detail( request, pk):
     list = get_object_or_404( List, pk=pk)
     items = Item.objects.filter( list_id = pk )
     return render(request, 'listApp/list_detail.html', {'list': list, 'items': items})
+
+def item_new( request ):
+    if request.method == "POST":
+        form = ItemForm(request.POST)
+        if form.is_valid():
+            item = form.save(commit=False)
+            item.created_date = timezone.now()
+            item.save()
+            return redirect('list_detail', pk = item.list.id)   
+    else:
+        form = ItemForm()
+    return render(request, 'listApp/item_edit.html', {'form': form})
+
+def item_edit(request, pk):
+    item = get_object_or_404(Item, pk=pk)
+    if request.method == "POST":
+        form = ItemForm(request.POST, instance=item)
+        if form.is_valid():
+            item = form.save(commit=False)
+            item.save()
+            return redirect('list_detail', pk=item.list.pk)
+    else:
+        form = ItemForm(instance=item)
+    return render(request, 'listApp/item_edit.html', {'form': form})
