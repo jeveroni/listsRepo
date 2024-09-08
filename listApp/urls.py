4fe307459611385958f11a37a1efd5e464c4c5cb
Jev Gore-Samad <jevgore-samad@jevs-mbp-2.home>
2024-09-08 21:21:23 +0100

    changing to keep listApp behind /list

--- listApp/urls.py
+++ listApp/urls.py
@@ -4,11 +4,11 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.list_list, name='list_list'),
+    path('list/', views.list_list, name='list_list'),
     path('list/<int:pk>/', views.list_detail, name='list_detail'),
     path('list/new/', views.list_new, name='list_new'),
     path('list/<int:pk>/item_new', views.item_new, name='item_new'),
-    path('item/<int:pk>/edit', views.item_edit, name='item_edit'),
+    path('list/item/<int:pk>/edit', views.item_edit, name='item_edit'),
     path('test/', views.test_page, name = 'test_page'),
-    path('update-item/', csrf_exempt(views.update_item), name='update_item'),
+    path('list/update-item/', csrf_exempt(views.update_item), name='update_item'),
 ]
